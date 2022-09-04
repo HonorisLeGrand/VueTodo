13ad390fa604b4c8cc7e36ab31972c24b65cdc56
CubahiroHonoris <cubahiroalx@gmail.com>
2022-09-04 09:39:25 +0200

    checking todo option

--- vuetodo/src/App.vue
+++ vuetodo/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+
+
   <div class="badass-todo">
     <div class="title has-text-centered">
       Badass-todo
@@ -15,15 +18,15 @@
   </p>
 </div>
 </form>
-<div v-for="i in todo" class="card my-3">
+<div v-for="i in todo" class="card my-3" :class="{'has-background-success-light' : i.done } ">
   <div class="card-content">
     <div class="content">
       <div class="columns is-mobile is-vcentered">
-        <div class="column ">
+        <div class="column " :class="{'has-text-success line-through'  : i.done}">
         {{ i.content }}
         </div>
         <div class="column is-5 has-text-right">
-          <button class="button is-light">
+          <button @click="toggleTodo(i.id)"  class="button mr-1" :class="i.done ? 'is-success': 'is-light'">
           &check;
           </button>
           <button @click="deleteTodo(i.id)" class="button is-danger">
@@ -39,10 +42,11 @@
   </div>
 </div>
   </div>
+</div>
 </template>
 
 <script setup>
-import { ref  } from "vue"
+import { ref, transformVNodeArgs  } from "vue"
 import { v4 as uuidv4 } from 'uuid'
 
 const todo = ref([
@@ -54,7 +58,7 @@ const todo = ref([
   ,  {
     id:"id2",
     content:"second todo",
-    done:false
+    done:true
   },  {
     id:"id3",
     content:"third todo",
@@ -75,6 +79,10 @@ const addTodo = () =>{
   const deleteTodo = (id) =>{
       todo.value = todo.value.filter((todo)=>todo.id !==id) 
   }
+  const toggleTodo = (id) =>{
+    const index = todo.value.findIndex((todo) => todo.id===id)
+    todo.value[index].done =!todo.value[index].done
+  }
 </script>
 
 
@@ -83,6 +91,9 @@ const addTodo = () =>{
 .badass-todo{
   width: 500px;
   margin: 20px auto;
+}
+.line-through{
+  text-decoration: line-through;
 }
 
 </style>
